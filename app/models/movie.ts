7c553a2dab46ffd8ce58564793fc6d73c8379382
momomak <m.makria@gmail.com>
2025-12-26 23:17:40 +0100

    Singleton Services and the Idea of Caching

--- app/models/movie.ts
+++ app/models/movie.ts
@@ -1,3 +1,4 @@
+import cache from '#services/cache_service'
 import { MovieService } from '#services/movie_service'
 import { toHtml } from '@dimerapp/markdown/utils'
 
@@ -20,12 +21,18 @@ export default class Movie {
   }
 
   static async find(slug: string) {
+    if (cache.has(slug)) {
+      console.log(`Cache got hit : ${slug}`)
+      return cache.get(slug)
+    }
     const md = await MovieService.read(slug)
     const movie = new Movie()
     movie.title = md.frontmatter.title
     movie.summary = md.frontmatter.summary
     movie.slug = slug
     movie.abstract = toHtml(md).contents
+
+    cache.set(slug, movie)
 
     return movie
   }
