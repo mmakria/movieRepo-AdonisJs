4b95c1ff0256472adb2eb97f82a593fcd6e4fbaf
momomak <m.makria@gmail.com>
2026-01-07 09:14:05 +0100

    mise en place du cache avec redis

--- app/models/movie.ts
+++ app/models/movie.ts
@@ -21,7 +21,7 @@ export default class Movie {
   }
 
   static async find(slug: string) {
-    if (cache.has(slug)) {
+    if (await cache.has(slug)) {
       console.log(`Cache got hit : ${slug}`)
       return cache.get(slug)
     }
@@ -32,7 +32,7 @@ export default class Movie {
     movie.slug = slug
     movie.abstract = toHtml(md).contents
 
-    cache.set(slug, movie)
+    await cache.set(slug, movie)
 
     return movie
   }
--- app/services/cache_service.ts
+++ app/services/cache_service.ts
@@ -1,20 +1,27 @@
+import redis from '@adonisjs/redis/services/main'
+
 class CacheService {
   #store: Record<string, any> = {}
 
-  has(key: string) {
-    return key in this.#store
+  async has(...keys: string[]) {
+    return await redis.exists(keys)
   }
 
-  get(key: string) {
-    return this.#store[key]
+  async get(key: string) {
+    const value = await redis.get(key)
+    return value && JSON.parse(value)
   }
 
-  set(key: string, value: any) {
-    this.#store[key] = value
+  async set(key: string, value: any) {
+    return redis.set(key, JSON.stringify(value))
   }
 
-  delete(key: string) {
-    delete this.#store[key]
+  async delete(...keys: string[]) {
+    return redis.del(keys)
+  }
+
+  async flushDb() {
+    return redis.flushdb()
   }
 }
 
